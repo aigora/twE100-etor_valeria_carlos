8dd0c8801b7c8385eba86621bde874003b40ebbb
EtorGP <47822078+EtorGonzalezPerea@users.noreply.github.com>
2019-04-26 18:40:03 +0200

    Prototipo Biblioteca
    Primeros pasos

---
+++ PruebaTrabajo1.c
@@ -0,0 +1,363 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// Declaración de estructura definida para USUARIOS
+typedef struct 
+{
+	int num_usuario;
+	char DNI[100];
+	char nombre[100];
+	char apellido[100];
+	char email[100];
+} Usuario;
+
+// Declaración de estructura definida para LIBROS
+typedef struct 
+{
+	int num_libro;	
+	char ISBN[100];
+	char titulo[100];
+	char genero[100];
+	char autor[100];	
+} Libro;
+
+// Declaración de estructura definida para PRESTAMOS
+typedef struct 
+{
+	int num_prestamo;
+	char DNI[100];
+	char ISBN[100];
+	char fecha_prestamo[100];
+	char fecha_devolucion[100];	
+} Prestamo;
+
+
+// Declaración de prototipos de funciones
+void menu_general(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[]);
+void menu_usuarios(Usuario mis_usuarios[], Prestamo mis_prestamos[]);
+void menu_libros(Libro mis_libros[], Prestamo mis_prestamos[]);
+void menu_prestamos(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[]);
+void alta_usuario(Usuario mis_usuarios[]);
+void listado_usuarios_por_apellido(Usuario mis_usuarios[]);
+void grabar_usuarios_fichero(Usuario mis_usuarios[]);
+void leer_usuarios_fichero (Usuario mis_usuarios[]);
+void eliminar_espacios (char sLiteral[]);
+void ordenar_usuarios (Usuario mis_usuarios[]);
+int busqueda_usuarios(Usuario mis_usuarios[]);
+void modificacion_usuarios(Usuario mis_usuarios[], Prestamo mis_prestamos[]);
+void baja_usuarios(Usuario mis_usuarios[], Prestamo mis_prestamos[]);
+int comprobar_email(char email[]);
+void alta_libro(Libro mis_libros[]);
+void listado_libros_ordenado (Libro mis_libros[], int iOpcion);
+void grabar_libros_fichero(Libro mis_libros[]);
+void leer_libros_fichero (Libro mis_libros[]);
+void ordenar_libros_titulo (Libro mis_libros[]);
+void ordenar_libros_autor (Libro mis_libros[]);
+void ordenar_libros_genero (Libro mis_libros[]);
+int busqueda_libros(Libro mis_libros[]);
+void modificacion_libros(Libro mis_libros[], Prestamo mis_prestamos[]);
+void baja_libros(Libro mis_libros[], Prestamo mis_prestamos[]);
+void leer_prestamos_fichero (Prestamo mis_prestamos[]);
+void grabar_prestamos_fichero(Prestamo mis_prestamos[]);
+void prestamo_libro(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[]);
+void listado_prestamos_por_libro(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[]);
+void ordenar_prestamos (Prestamo mis_prestamos[]);
+void devolucion_libro(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[]);
+void consulta_prestamos_libro(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[]);
+void consulta_prestamos_usuario(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[]);
+
+
+
+//***********************************************************
+//**                                                       **
+//**                  FUNCION MAIN                         **
+//**                                                       **
+//***********************************************************
+int main()
+{
+	// Declaración de estructuras de Usuarios, Libros y Préstamos.  Estas estructuras se pasan en argumentos por referencia a las funciones que los necesitan
+    Usuario mis_usuarios[100];
+    Libro mis_libros[100]; 
+	Prestamo mis_prestamos[100];   
+    
+    // Carga inicial de las estructuras a partir de los datos almacenados en los ficheros correspondientes
+    leer_usuarios_fichero (mis_usuarios);
+    leer_libros_fichero (mis_libros);
+    leer_prestamos_fichero (mis_prestamos);    
+    
+    // Llamada al menú general
+    menu_general(mis_usuarios, mis_libros, mis_prestamos);
+    
+    // En lugar de cerrarse el programa, espera que se pulse una tecla para finalizar
+    system("pause");
+    
+    return 0;
+}
+
+
+
+//***********************************************************
+//**                                                       **
+//**                  FUNCION MENU_GENERAL                 **
+//**                                                       **
+//***********************************************************
+void menu_general(Usuario mis_usuarios[], Libro mis_libros[], Prestamo mis_prestamos[])
+{   
+int iOpcion;
+
+	
+	do{
+       
+    	// ciclo DO-WHILE hasta que se introduzca un número de opción válido
+        do {
+			system("cls");
+			printf("\n__________________BIBLIOTECA DE LA ETSIDI__________________________\n\n");		
+	    	printf("\n_______________________MENU PRINCIPAL______________________________\n\n");
+        	printf("\n1. Gestion de Usuarios.");
+        	printf("\n2. Gestion de Libros.");
+        	printf("\n3. Gestion de Prestamos.");
+	        printf("\n4. Salir.");
+    	    printf("\n___________________________________________________________________\n");
+        	printf("\n\n\nIntroduzca su opcion: ");
+    		fflush(stdin);        	
+        	scanf("%d",&iOpcion);
+        	printf("\n");
+        } while(iOpcion < 1 || iOpcion > 4);
+        
+        // Según la opción elegida, se llama a una u otra función.  A este nivel tan solo menús de usuarios, libros y préstamos
+        switch (iOpcion)
+		{
+            case 1:
+                
+				menu_usuarios(mis_usuarios, mis_prestamos);
+                
+            break;
+            
+            case 2:
+            	
+	            menu_libros(mis_libros, mis_prestamos);
+				
+            break;
+            
+            case 3:
+            	
+                menu_prestamos(mis_usuarios, mis_libros, mis_prestamos);
+				
+            break;	
+                        	
+			case 4:
+                
+				printf("\nSaliendo del programa.\n");
+				
+			break;				
+          
+            default:
+                printf("\nOpcion no valida\n");
+             
+        
+		}
+    } while (iOpcion!= 4);
+    
+			
+}
+
+
+//***********************************************************
+//**                                                       **
+//**                  FUNCION MENU_USUARIOS                **
+//**                                                       **
+//***********************************************************
+void menu_usuarios(Usuario mis_usuarios[], Prestamo mis_prestamos[])
+{   
+int iOpcion;
+int iResultado;
+
+	
+	do{
+       
+        // ciclo DO-WHILE hasta que se introduzca un número de opción válido
+        do {
+			system("cls");
+			printf("\n__________________BIBLIOTECA DE LA ETSIDI__________________________\n\n");		
+	    	printf("\n_______________________MENU USUARIOS_______________________________\n\n");
+        	printf("\n1. Alta de Usuario.");
+        	printf("\n2. Modificar datos de un Usuario.");
+        	printf("\n3. Borrar Usuario.");
+        	printf("\n4. Busqueda de Usuarios.");
+	        printf("\n5. Listado de Usuarios ordenado por Apellido.");
+ 	        printf("\n6. Volver a menu general.");
+    	    printf("\n___________________________________________________________________\n");
+        	printf("\n\n\nIntroduzca su opcion: ");
+			fflush(stdin);        	
+        	scanf("%d",&iOpcion);
+        	printf("\n");
+        } while(iOpcion < 1 || iOpcion > 6);
+
+        // Según la opción elegida, se llama a una u otra función.  A este nivel tan solo funciones relacionadas con gestión de USUARIOS        
+        switch (iOpcion)
+		{
+            case 1:
+                
+				alta_usuario(mis_usuarios);
+				// se graba en fichero cuando hay un alta, una modificación o una baja de usuarios
+				grabar_usuarios_fichero (mis_usuarios);
+                
+            break;
+            
+            case 2:
+            	
+				modificacion_usuarios(mis_usuarios, mis_prestamos);
+				// se graba en fichero cuando hay un alta, una modificación o una baja de usuarios				
+				grabar_usuarios_fichero (mis_usuarios);
+				
+            break;	
+                        
+			case 3:
+				
+				baja_usuarios(mis_usuarios, mis_prestamos);
+				// se graba en fichero cuando hay un alta, una modificación o una baja de usuarios
+				grabar_usuarios_fichero (mis_usuarios);
+			
+			break;	
+			
+			case 4:
+                
+				iResultado = busqueda_usuarios(mis_usuarios);
+                system("pause");				
+
+			break;	
+			
+			case 5:
+                
+				listado_usuarios_por_apellido(mis_usuarios);
+				// se graba en fichero cuando se reordena la estructura de usuarios por apellido				
+				grabar_usuarios_fichero (mis_usuarios);				
+				
+            break;
+					
+			case 6:
+                
+				printf("\n7. Volver a menu general.\n");
+				
+			break;				
+          
+            default:
+                printf("\nOpcion no valida\n");
+                system("pause");                
+        
+		}
+    } while (iOpcion!= 6);
+    
+			
+}
+
+
+//***********************************************************
+//**                                                       **
+//**                  FUNCION MENU_LIBROS                  **
+//**                                                       **
+//***********************************************************
+void menu_libros(Libro mis_libros[], Prestamo mis_prestamos[])
+{   
+int iOpcion;
+int iResultado;
+
+	
+	do{
+       
+        // ciclo DO-WHILE hasta que se introduzca un número de opción válido
+        do {
+			system("cls");
+			printf("\n__________________BIBLIOTECA DE LA ETSIDI__________________________\n\n");		
+	    	printf("\n_______________________MENU LIBROS_________________________________\n\n");
+        	printf("\n1. Alta de Libro.");
+        	printf("\n2. Modificar datos de un Libro.");
+        	printf("\n3. Borrar Libro.");
+        	printf("\n4. Consulta de datos de un Libro.");
+	        printf("\n5. Listado de Libros ordenado por Titulo.");
+    	    printf("\n6. Listado de Libros ordenado por Autor."); 
+    	    printf("\n7. Listado de Libros ordenado por Genero."); 					 
+	        printf("\n8. Volver a menu general.");
+    	    printf("\n___________________________________________________________________\n");
+        	printf("\n\n\nIntroduzca su opcion: ");
+        	fflush(stdin);        	
+        	scanf("%d",&iOpcion);
+        	printf("\n");
+        } while(iOpcion < 1 || iOpcion > 8);
+
+        // Según la opción elegida, se llama a una u otra función.  A este nivel tan solo funciones relacionadas con gestión de LIBROS             
+        switch (iOpcion)
+		{
+            case 1:
+                
+				alta_libro (mis_libros);
+				// se graba en fichero cuando hay un alta, una modificación o una baja de libros				
+				grabar_libros_fichero (mis_libros);
+                
+            break;
+            
+            case 2:
+            	
+				modificacion_libros (mis_libros, mis_prestamos);
+				// se graba en fichero cuando hay un alta, una modificación o una baja de libros					
+				grabar_libros_fichero (mis_libros);
+				
+            break;	
+                        
+			case 3:
+				
+				baja_libros (mis_libros, mis_prestamos);
+				// se graba en fichero cuando hay un alta, una modificación o una baja de libros					
+				grabar_libros_fichero (mis_libros);
+			
+			break;	
+			
+			case 4:
+                
+				iResultado = busqueda_libros (mis_libros);
+                system("pause");
+
+			break;	
+			
+			case 5:
+                
+                listado_libros_ordenado (mis_libros, 1);
+				// se graba en fichero cuando se reordena la estructura de libros     
+				grabar_libros_fichero (mis_libros);		
+				
+            break;
+			
+			case 6:
+                
+                listado_libros_ordenado (mis_libros, 2);
+				// se graba en fichero cuando se reordena la estructura de libros                    
+				grabar_libros_fichero (mis_libros);	                
+				
+			break;
+			
+			case 7:
+                
+                listado_libros_ordenado (mis_libros, 3);
+				// se graba en fichero cuando se reordena la estructura de libros                    
+				grabar_libros_fichero (mis_libros);	                
+				
+			break;					
+				
+			case 8:
+                
+				printf("\n7. Volver a menu general.\n");
+				
+			break;				
+          
+            default:
+                printf("\nOpcion no valida\n");
+                system("pause");                
+        
+		}
+    } while (iOpcion!= 8);
+    
+			
+}
+
